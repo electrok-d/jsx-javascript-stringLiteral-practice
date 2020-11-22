ac291221cb7a8f41f973f945688321c6e4ec7800
Jed Black <62964873+electrok-d@users.noreply.github.com>
2020-11-22 14:35:57 -0800

    Created variable and injected them into ReactDOM.render using string literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Create a react app from scratch.
+// Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
@@ -6,3 +6,19 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Jed";
+const lName = "Black";
+const d = new Date();
+
+ReactDOM.render(
+  <div>
+    <p>Built by {`${name} ${lName}`}</p>
+    <p>Copyright - {d.getFullYear()}</p>
+  </div>,
+
+  document.getElementById("root")
+);
